6b4f8db27e4e46bbbaef6e1acdc1446aa37863eb
MaheshB77 <mbansode7696@gmail.com>
2021-04-25 12:19:30 +0530

    Added user object listener

--- src/main/java/com/kafka/consumer/demo/kafkaconsumer/listener/KafkaConsumer.java
+++ src/main/java/com/kafka/consumer/demo/kafkaconsumer/listener/KafkaConsumer.java
@@ -1,13 +1,21 @@
 package com.kafka.consumer.demo.kafkaconsumer.listener;
 
+import com.kafka.consumer.demo.kafkaconsumer.models.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class KafkaConsumer {
 
+//    @KafkaListener(topics = "KafkaExample", groupId = "group_id")
+//    public void consumeMessage(String message) {
+//        System.out.println("Consumed message : " + message);
+//    }
+
     @KafkaListener(topics = "KafkaExample", groupId = "group_id")
-    public void consumeMessage(String message) {
-        System.out.println("Consumed message : " + message);
+    public void consumeUser(List<User> users) {
+        System.out.println("Consumed user : " + users);
     }
 }
